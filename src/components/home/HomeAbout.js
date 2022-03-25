d4c32b301224e0707fa23242c117946de3c64624
jacpolanski <jac.polanski@gmail.com>
2022-03-25 14:54:15 +0100

    Added about us section, small styling adjustments

--- src/components/home/HomeAbout.js
+++ src/components/home/HomeAbout.js
@@ -1,9 +1,22 @@
 import React from "react";
+import Decoration from "../other/Decoration";
 
 function HomeAbout() {
   return (
-    <div id="about" className="element">
-      HomeAbout
+    <div id="about" className="element about-wrapper">
+      <div className="about-text">
+        <div className="about-text_wrapper">
+          <div className="about-text_header">O nas</div>
+          <Decoration />
+          <div className="about-text_paragraph">
+            Nori grape silver beet broccoli kombu beet greens fava bean potato
+            quandong celery. Bunya nuts black-eyed pea prairie turnip leek
+            lentil turnip greens parsnip.
+          </div>
+          <div className="about-text_signature" />
+        </div>
+      </div>
+      <div className="about-image" />
     </div>
   );
 }
