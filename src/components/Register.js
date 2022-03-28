b2cbfe3a1667e2d885c6547ce4623da15606792c
jacpolanski <jac.polanski@gmail.com>
2022-03-28 19:24:06 +0200

    Added Register page route and it's styling. Minor styling changes.

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,7 +1,46 @@
 import React from "react";
+import Decoration from "./other/Decoration";
+import { Link } from "react-router-dom";
 
-function Register(props) {
-  return <div>This is Register Page</div>;
+function Register() {
+  return (
+    <section className="register-wrapper" id="register">
+      <div className="register-text">
+        <div className="register-text_head">Załóż konto</div>
+        <Decoration />
+        <form className="form">
+          <div className="form-field">
+            <label className="form-label" htmlFor="email">
+              Email
+            </label>
+            <input type="email" className="form-input" id="email" />
+          </div>
+          <div className="form-field">
+            <label className="form-label" htmlFor="password">
+              Hasło
+            </label>
+            <input type="password" className="form-input" id="password" />
+          </div>
+          <div className="form-field">
+            <label className="form-label" htmlFor="password-repeat">
+              Powtórz hasło
+            </label>
+            <input
+              type="password"
+              className="form-input"
+              id="password-repeat"
+            />
+          </div>
+        </form>
+        <div className="register-button-wrapper">
+          <Link to="/logowanie">
+            <button className="button-mid">Zaloguj się</button>
+          </Link>
+          <button className="button-mid">Załóż konto</button>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Register;
