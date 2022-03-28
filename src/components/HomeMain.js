7b3c20c58f04ca0927ae821407d8e64802273ef2
jacpolanski <jac.polanski@gmail.com>
2022-03-28 18:45:29 +0200

    Added LogIn page rout and it's styling. Minor styling changes.

--- src/components/HomeMain.js
+++ src/components/HomeMain.js
@@ -4,7 +4,7 @@ import HomeReason from "./home/HomeReason";
 import HomeOrganizations from "./home/HomeOrganizations";
 import HomeContact from "./home/HomeContact";
 import HomeHeader from "./home/HomeHeader";
-import ScrollArrow from "../scss/components/ScrollArrow";
+import ScrollArrow from "./other/ScrollArrow";
 
 function HomeMain() {
   return (
--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,7 +1,36 @@
 import React from "react";
+import Decoration from "./other/Decoration";
+import { Link } from "react-router-dom";
 
-function LogIn(props) {
-  return <div>This is LogIn Pages</div>;
+function LogIn() {
+  return (
+    <section className="logIn-wrapper" id="LogIn">
+      <div className="logIn-text">
+        <div className="logIn-text_head">Skontaktuj się z nami</div>
+        <Decoration />
+        <form className="form">
+          <div className="form-field">
+            <label className="form-label" htmlFor="email">
+              Email
+            </label>
+            <input type="email" className="form-input" id="email" />
+          </div>
+          <div className="form-field">
+            <label className="form-label" htmlFor="password">
+              Hasło
+            </label>
+            <input type="password" className="form-input" id="password" />
+          </div>
+        </form>
+        <div className="logIn-button-wrapper">
+          <Link to="/rejestracja">
+            <button className="button-mid">Załóż konto</button>
+          </Link>
+          <button className="button-mid">Zaloguj się</button>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default LogIn;
