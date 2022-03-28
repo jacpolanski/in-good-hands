2279efccca0035b62ad0b2e5effda78468128018
jacpolanski <jac.polanski@gmail.com>
2022-03-28 19:35:10 +0200

    Added LogOut page route and it's styling. Minor styling changes.

--- src/components/LogOut.js
+++ src/components/LogOut.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Decoration from "./other/Decoration";
+import { Link } from "react-router-dom";
 
-function LogOut(props) {
-  return <div>This is LogOut page</div>;
+function LogOut() {
+  return (
+    <section className="logOut-wrapper" id="logOut">
+      <div className="logOut-text">
+        <div className="logOut-text_head">Wylogowanie nastąpiło pomyślnie!</div>
+        <Decoration />
+        <div className="logOut-button-wrapper">
+          <Link to="/">
+            <button className="button-mid">Strona główna</button>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default LogOut;
