e171aa19a041ef0579f849aea29620263d4bac1b
jacpolanski <jac.polanski@gmail.com>
2022-03-29 23:59:16 +0200

    Contact form validation finished. Send contact request by POST added. Minor styling adjustments.

--- src/components/home/HomeContact.js
+++ src/components/home/HomeContact.js
@@ -1,15 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import Decoration from "../other/Decoration";
 import Form from "./contact/Form";
 import Footer from "./contact/Footer";
 
 function HomeHeader() {
+  const [isSent, setIsSent] = useState(false);
+
+  console.log(isSent);
   return (
     <section className="element contact-wrapper" id="contact">
       <div className="contact-text">
         <div className="contact-text_head">Skontaktuj się z nami</div>
-        <Decoration />
-        <Form />
+        <div className="contact-sent-wrapper">
+          <Decoration />
+          <div className={isSent ? "contact-sent active" : "contact-sent"}>
+            <span>Wiadomość została wysłana.</span>
+            <span>Wkrótce się skontaktujemy</span>
+          </div>
+        </div>
+        <Form setIsSent={setIsSent} />
       </div>
       <footer>
         <Footer />
--- src/components/home/contact/Form.js
+++ src/components/home/contact/Form.js
@@ -1,9 +1,62 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { schemaContact } from "./schemaContact";
+import uniqid from "uniqid";
 
-function Form() {
+function Form({ setIsSent }) {
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const [errors, setErrors] = useState([]);
+  const [errorsMsgs, setErrorMsgs] = useState({
+    name: [],
+    email: [],
+    message: [],
+  });
+
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    setIsSent(false);
+    e.preventDefault();
+
+    const { error } = schemaContact.validate(form, {
+      abortEarly: false,
+    });
+    error !== undefined && setErrors(error.details);
+
+    error === undefined &&
+      fetch(`https://fer-api.coderslab.pl/v1/portfolio/contact`, {
+        method: "POST",
+        body: JSON.stringify(form),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          data.status === "success" && setIsSent(true);
+        });
+    error === undefined && setForm({ name: "", email: "", message: "" });
+  };
+
+  useEffect(() => {
+    errors.length !== 0 &&
+      setErrorMsgs({
+        name: errors.filter((error) => error.context.key === "name"),
+        email: errors.filter((error) => error.context.key === "email"),
+        message: errors.filter((error) => error.context.key === "message"),
+      });
+  }, [errors]);
+
+  console.log(errorsMsgs);
   return (
     <div className="form-wrapper">
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <div className="form-field-wrapper">
           <div className="form-field">
             <label className="form-label" htmlFor="name">
@@ -11,21 +64,43 @@ function Form() {
             </label>
             <input
               type="text"
+              name="name"
+              value={form.name}
+              onChange={handleChange}
               className="form-input"
               id="name"
               placeholder="Krzysztof"
             />
+            <div className="name-errors">
+              <ul>
+                {errorsMsgs.name.length !== 0 &&
+                  errorsMsgs.name.map((msg) => (
+                    <li key={uniqid()}>{msg.message}</li>
+                  ))}
+              </ul>
+            </div>
           </div>
           <div className="form-field">
             <label className="form-label" htmlFor="email">
               Wpisz swój email
             </label>
             <input
-              type="email"
+              type="text"
+              name="email"
+              value={form.email}
+              onChange={handleChange}
               className="form-input"
               id="email"
               placeholder="abc@xyz.pl"
             />
+            <div className="email-errors">
+              <ul>
+                {errorsMsgs.email.length !== 0 &&
+                  errorsMsgs.email.map((msg) => (
+                    <li key={uniqid()}>{msg.message}</li>
+                  ))}
+              </ul>
+            </div>
           </div>
         </div>
 
@@ -34,10 +109,21 @@ function Form() {
             Wpisz swoją wiadomość
           </label>
           <textarea
+            name="message"
+            value={form.message}
+            onChange={handleChange}
             className="form-input text"
-            id="text"
+            id="message"
             placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
           />
+          <div className="text-errors">
+            <ul>
+              {errorsMsgs.message.length !== 0 &&
+                errorsMsgs.message.map((msg) => (
+                  <li key={uniqid()}>{msg.message}</li>
+                ))}
+            </ul>
+          </div>
         </div>
 
         <button type="submit" className="button-mid">
