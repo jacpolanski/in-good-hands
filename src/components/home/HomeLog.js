099f07a1d9243a1cbf80208b05b44ed8a4974f70
jacpolanski <jac.polanski@gmail.com>
2022-04-02 16:38:02 +0200

    User logout feature added

--- src/components/home/HomeLog.js
+++ src/components/home/HomeLog.js
@@ -1,17 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { useAuth } from "../other/AuthContext";
+import { auth } from "../../firebase";
 
 function HomeLog() {
-  return (
-    <div className="home-log">
-      <Link to="/logowanie">
-        <button className="button-small">Zaloguj</button>
-      </Link>
-      <Link to="/rejestracja">
-        <button className="button-small">Załóż konto</button>
-      </Link>
-    </div>
-  );
+  const { currentUser, logOut } = useAuth();
+  if (!currentUser) {
+    return (
+      <div className="home-log">
+        <Link to="/logowanie">
+          <button className="button-small">Zaloguj</button>
+        </Link>
+        <Link to="/rejestracja">
+          <button className="button-small">Załóż konto</button>
+        </Link>
+      </div>
+    );
+  } else {
+    return (
+      <div className="home-log bu">
+        <span className="button-small">Cześć {currentUser.email}</span>
+        <Link to="#">
+          <button className="button-small">Oddaj rzeczy</button>
+        </Link>
+        <button className="button-small" onClick={() => logOut(auth)}>
+          Wyloguj
+        </button>
+      </div>
+    );
+  }
 }
 
 export default HomeLog;
